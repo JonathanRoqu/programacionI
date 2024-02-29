3f7de116248c2a73d06c9065c7d15e8350d07941
JonathanRoqu <158531919+JonathanRoqu@users.noreply.github.com>
2024-02-28 19:01:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/conversores2/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.conversores2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabHost tbh;
+    TextView tempVal;
+    Button btn;
+    Spinner spn;
+    conversores objConversor = new conversores();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tbh = findViewById(R.id.tbhConversores);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("LONGITUD").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("ALMACENAMIENTO").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
+        tbh.addTab(tbh.newTabSpec("MONEDAS").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
+
+        btn = findViewById(R.id.btnLongitudConvertir);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //Conversor de Longitud
+                spn = findViewById(R.id.spnLongitudDe);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnLongitudA);
+                int a = spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtLongitudCantidad);
+                double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+                double resp = objConversor.convertir(0, de, a, cantidad);
+                Toast.makeText(getApplicationContext(), "Respuesta: " +
+                        resp, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
+class conversores{ //Longitud
+    double[][] valores = {
+            {1,100, 39.3701, 3.280841666667, 1.193, 1.0936138888889999077,
+                    0.001, 0.000621371}
+    };
+    public double convertir(int opcion, int de, int a, double cantidad){
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
+
+    }
+}
